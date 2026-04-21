099ba0c41db98dd447d4bbb24e8f1c8284e66a05
wojciesze21 <iszczak.wojciech@gmail.com>
2026-04-21 17:47:41 +0200

    Ball move test methods checks collision with box

--- ConcurrentProgramming/ReactiveInteractiveUserInterface/DataTest/BallUnitTest.cs
+++ ConcurrentProgramming/ReactiveInteractiveUserInterface/DataTest/BallUnitTest.cs
@@ -21,16 +21,80 @@ namespace TP.ConcurrentProgramming.Data.Test
     }
 
     [TestMethod]
-    public void MoveTestMethod()
+    public void MoveOnBoundaryTestMethod()
     {
-      Vector initialPosition = new(10.0, 10.0);
-      Ball newInstance = new(initialPosition, new Vector(0.0, 0.0));
-      IVector curentPosition = new Vector(0.0, 0.0);
-      int numberOfCallBackCalled = 0;
-      newInstance.NewPositionNotification += (sender, position) => { Assert.IsNotNull(sender); curentPosition = position; numberOfCallBackCalled++; };
-      newInstance.Move(new Vector(0.0, 0.0));
-      Assert.AreEqual<int>(1, numberOfCallBackCalled);
-      Assert.AreEqual<IVector>(initialPosition, curentPosition);
+
+      Vector positionLimiter = new Vector(400, 400);
+      int callbackCount = 0;
+
+      void RunTest(Vector initialPosition, Vector velocity)
+      {
+        Ball ball = new Ball(initialPosition, velocity);
+
+        IVector currentPosition = null;
+
+        ball.NewPositionNotification += (sender, position) =>
+        {
+          currentPosition = position;
+          callbackCount++;
+        };
+
+        Vector delta = new Vector(velocity.x, velocity.y);
+
+        ball.Move(delta, positionLimiter);
+
+        Assert.IsNotNull(currentPosition);
+
+        Assert.IsTrue(currentPosition.x >= 0 && currentPosition.x <= positionLimiter.x);
+        Assert.IsTrue(currentPosition.y >= 0 && currentPosition.y <= positionLimiter.y);
+      }
+
+
+      RunTest(new Vector(390, 200), new Vector(20, 0));
+
+      RunTest(new Vector(10, 200), new Vector(-20, 0));
+
+      RunTest(new Vector(200, 390), new Vector(0, 20));
+
+      RunTest(new Vector(200, 10), new Vector(0, -20));
+      
+      Assert.AreEqual(4, callbackCount);
+    }
+
+    [TestMethod]
+    public void MoveCornerCollisionTestMethod()
+    {
+      Vector positionLimiter = new Vector(400, 400);
+
+      Vector initialPosition = new Vector(390, 390);
+      
+      Vector velocity = new Vector(20, 20);
+
+      Ball ball = new Ball(initialPosition, velocity);
+
+      IVector currentPosition = null;
+      int callbackCount = 0;
+
+      ball.NewPositionNotification += (sender, position) =>
+      {
+        currentPosition = position;
+        callbackCount++;
+      };
+
+      Vector delta = new Vector(velocity.x, velocity.y);
+      
+      ball.Move(delta, positionLimiter);
+      
+      Assert.IsNotNull(currentPosition);
+      Assert.AreEqual(1, callbackCount);
+      
+      Assert.IsTrue(currentPosition.x <= positionLimiter.x);
+      Assert.IsTrue(currentPosition.y <= positionLimiter.y);
+
+      // powinna zmienić kierunek (odbicie)
+      Assert.IsTrue(Math.Abs(currentPosition.x - (initialPosition.x - delta.x)) < 0.01 &&
+                    Math.Abs(currentPosition.y - (initialPosition.y - delta.y)) < 0.01);
     }
   }
+
 }
